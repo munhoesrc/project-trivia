c9dea48221607bf0629451af54ef9c802090dd43
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-10-11 19:47:53 -0300

    Requisito 10

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,8 +1,9 @@
-import { GET_LOGIN } from '../actions/actionsTypes';
+import { GET_LOGIN, SET_SCORE } from '../actions/actionsTypes';
 
 const INITIAL_STATE = {
   email: '',
   name: '',
+  score: 0,
 };
 
 const player = (state = INITIAL_STATE, action) => {
@@ -12,6 +13,11 @@ const player = (state = INITIAL_STATE, action) => {
       ...state,
       email: action.email,
       name: action.name,
+    };
+  case SET_SCORE:
+    return {
+      ...state,
+      score: state.score + action.payload,
     };
   default:
     return state;
