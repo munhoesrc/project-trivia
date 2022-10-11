0f168acc81780e3b12de78fb3f34e2e212de9040
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-10-10 22:40:39 -0300

    Requisito 06 e 07 Done + CSS botão correto e incorreto

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Questions from '../components/Questions';
+import { fetchPerguntas } from '../redux/actions/index';
 
 class Game extends Component {
+  state = {
+    carregando: true,
+  };
+
+  componentDidMount() {
+    const { getPerg } = this.props;
+    getPerg();
+  }
+
+  componentDidUpdate() {
+    const { req } = this.props;
+    const { carregando } = this.state;
+    this.validationLogout();
+    if (Object.keys(req).length > 0 && carregando) {
+      this.setState({ carregando: false });
+    }
+  }
+
+  validationLogout = () => {
+    const { req: { response_code: responseCode }, history } = this.props;
+    const codeExpirado = 3;
+    if (responseCode === codeExpirado) {
+      history.push('/');
+    }
+  };
+
   render() {
+    const { carregando } = this.state;
     return (
       <>
         <Header />
-        <div>Game</div>
+        <section>
+          { !carregando && <Questions /> }
+          {carregando && <h1>Carregando...</h1>}
+        </section>
       </>
     );
   }
 }
 
-export default Game;
+const mapDispatchToProps = (dispatch) => ({
+  getPerg: () => dispatch(fetchPerguntas()),
+});
+
+const mapStateToProps = (state) => ({
+  req: state.perg,
+});
+
+Game.propTypes = {
+  getPerg: PropTypes.func.isRequired,
+  req: PropTypes.objectOf(PropTypes.shape).isRequired,
+  history: PropTypes.objectOf(PropTypes.shape).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Game);
